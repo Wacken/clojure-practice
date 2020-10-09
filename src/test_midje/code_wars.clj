f31f9f987bed262d3e4facf96101f9491cdc7b8b
wacken <sebastianwalchi@gmail.com>
2020-10-09 08:35:52 +0200

    sensible ignore and some kata wars code

--- src/test_midje/code_wars.clj
+++ src/test_midje/code_wars.clj
@@ -12,3 +12,20 @@
                in)
           (map #(s/join (rest %))
                in)))))
+
+(reduce + [1 2 3])
+
+(fn fib [n]
+  (cond (or (= n 1) (= n 2)) 1
+        :else
+        (+ (fib (- n 1)) (fib (- n 2)))))
+
+(defn fibSeq [n]
+  (map (fn fib [n]
+         (cond (or (= n 1) (= n 2)) 1
+               :else
+               (+ (fib (- n 1)) (fib (- n 2)))))
+       (map inc (take n (range)))))
+
+(defn palindrome? [input]
+  (= (reverse (reverse input)) (reverse input)))
